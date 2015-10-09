a3df7728d3da1e210e25992e158687d9823a4fc5
andreylimonov <demonftp@yandex.ru>
2015-10-09 19:27:01 +0300

    Работает

---
+++ Python Lists and Dictionaries/11_14.py
@@ -0,0 +1,15 @@
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+
+# Your code here: Add some dish-price pairs to menu!
+
+menu['Spam'] = 2.50
+print menu['Spam']
+menu['Job'] = 3.50
+print menu['Job']
+menu['Soup'] = 0.99
+print menu['Soup']
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
